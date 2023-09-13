6270b3f7c56f83767fe93c2504e4b963dbc9d741
Amir Zhumagaliev <leasholea@gmail.com>
2023-09-13 10:21:51 +0600

    Добавьте файлы проекта.

---
+++ Module1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task5();
+            task6();
+        }
+        public static void task6()
+        {
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b + c);
+        }
+        public static void task5()
+        {
+            string name = "", surname = "";
+            Console.Write("Write your name: ");
+            name = Console.ReadLine();
+            Console.Write("Write your surname: ");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("Приветствую тебя - " + name + surname);
+        }
+    }
+}
